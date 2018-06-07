2dc8e644e7034f21f71dca9e4dbb7e8b42c40cb2
Vijayaprakash P <msgtovp@gmail.com>
2018-06-07 16:25:30 +0530

    Array of Object(s) Rendered in Web Page

--- js/app.js
+++ js/app.js
@@ -4,14 +4,40 @@
     app.controller("PostController", function() {
         console.log("Post Invoked");
 
-        var post = {
+        var post = [ {
             'title': 'MY First Post',
             'author': 'Guest',
             'content': 'This is my first post data using JS',
             'date_posted': '12-05-2018'
-        }
+            }, {
+                'title': 'MY Second Post',
+                'author': 'Guest',
+                'content': 'This is my second post data using JS',
+                'date_posted': '16-05-2018'
+            }, {
+                'title': 'MY First Post',
+                'author': 'Guest',
+                'content': 'This is my first post data using JS',
+                'date_posted': '12-05-2018'
+            }, {
+                'title': 'MY First Post',
+                'author': 'Guest',
+                'content': 'This is my first post data using JS',
+                'date_posted': '12-05-2018'
+            }, {
+                'title': 'MY First Post',
+                'author': 'Guest',
+                'content': 'This is my first post data using JS',
+                'date_posted': '12-05-2018'
+            }, {
+                'title': 'MY First Post',
+                'author': 'Guest',
+                'content': 'This is my first post data using JS',
+                'date_posted': '12-05-2018'
+            }
+        ];
 
-        this.topic = post;
+        this.topics = post;
     });
 
 })();
