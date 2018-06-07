d1af45aa60655570f7cdab9b60987f2f20187ccf
Vijayaprakash P <msgtovp@gmail.com>
2018-06-07 18:33:15 +0530

    CRUD With AJAX REST API Added into project

--- js/app.js
+++ js/app.js
@@ -1,40 +1,50 @@
-(function() {
+(function () {
     var app = angular.module('blog', []);
 
-    app.controller("PhoneController", function() {
-        
-        var contacts = [
-            {'name': 'Name 10', 'number': '+91-9876543210'},
-            {'name': 'Name 11', 'number': '+91-9876543211'},
-            {'name': 'Name 12', 'number': '+91-9876543212'},
-            {'name': 'Name 13', 'number': '+91-9876543213'},
-            {'name': 'Name 14', 'number': '+91-9876543214'},
-            {'name': 'Name 15', 'number': '+91-9876543215'}
-        ]
+    app.controller("PhoneController", ['$http', function ($http) {
 
-        this.books = contacts;
+        var cur = this;
 
-        this.toggleEdit = function(index) {
-            if(this.books[index].status) {
-                this.books[index].status = false;
-            } else {
+        this.books = [];
+
+        $http.get('http://localhost:3000/books').then(function (res) {
+            cur.books = res.data
+        }, function () {});
+
+        this.toggleEdit = function (id, index) {
+            if (!this.books[index].status) {
                 this.books[index].status = true;
-                //Do update by AJAX
+            } else {
+                this.books[index].status = false;
             }
         };
 
+        this.updateBook = function (id, index) {
+            delete cur.books[index].status
+            $http.put('http://localhost:3000/books/' + id, cur.books[index]).then(function (res) {
+                cur.books[index].status = false;
+            }, function () {});
+        };
+
         //var cur = this;
-        this.deleteContact = function(index) {
+        this.deleteContact = function (id, index) {
             //console.log(cur.books[index])
-            //Do delete by AJAX
-            this.books.splice(index, 1);
+            $http.delete('http://localhost:3000/books/' + id).then(function (res) {
+                cur.books.splice(index, 1);
+            }, function () {});
+
         };
 
-        this.addNewPhone = function() {
-            this.books.push(this.rec);
-            this.rec = {}
+        this.addNewPhone = function () {
+            var now = new Date()
+            cur.rec.id = now.getTime()
+            $http.post('http://localhost:3000/books', cur.rec).then(function (res) {
+                cur.books.push(cur.rec);
+            }, function () {});
+
+            cur.rec = {}
         };
 
-    });
+    }]);
 
 })();
