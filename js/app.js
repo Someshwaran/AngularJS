4de1a6812f20fa3cfd349df2d9f132186f45455c
Vijayaprakash P <msgtovp@gmail.com>
2018-06-07 16:15:20 +0530

    Object Rendered in Web Page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('blog', []);
+(function() {
+    var app = angular.module('blog', []);
+
+    app.controller("PostController", function() {
+        console.log("Post Invoked");
+
+        var post = {
+            'title': 'MY First Post',
+            'author': 'Guest',
+            'content': 'This is my first post data using JS',
+            'date_posted': '12-05-2018'
+        }
+
+        this.topic = post;
+    });
+
+})();
